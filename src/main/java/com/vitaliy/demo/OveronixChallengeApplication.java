14ab86d42ec0196d96c283cea9d43b7ff756cc29
Vitalij Stinskij <vstinskij@gmail.com>
2021-07-28 19:28:19 +0300

    initial commit

---
+++ src/main/java/com/vitaliy/demo/OveronixChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.vitaliy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OveronixChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OveronixChallengeApplication.class, args);
+	}
+
+}
