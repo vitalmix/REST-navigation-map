63e9ad023c10959f1d1922bcd23f17b5b2b04b53
Vitalij Stinskij <vstinskij@gmail.com>
2021-08-12 01:18:02 +0300

    added SecurityWebApplicationInitializer

---
+++ src/main/java/com/vitaliy/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.vitaliy.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
